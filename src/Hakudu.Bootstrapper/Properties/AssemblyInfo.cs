7a173b68eff929523e50fe11756ab4d733843ba6
Sergey Kostrukov <kostrse@gmail.com>
2016-03-02 02:16:54 +0800

    Build versioning and packaging

--- src/Hakudu.Bootstrapper/Properties/AssemblyInfo.cs
+++ src/Hakudu.Bootstrapper/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Hakudu Project")]
 [assembly: AssemblyCopyright("Copyright 2016 Hakudu Project")]
 
-[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("0.1.0-dev")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
