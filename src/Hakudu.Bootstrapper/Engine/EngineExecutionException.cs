597059a44f515fe71ecbd3f4c5760fbfe40c98ed
Sergey Kostrukov <kostrse@gmail.com>
2016-03-04 03:50:58 +0800

    Host for execution of the child Hakudu engine process

---
+++ src/Hakudu.Bootstrapper/Engine/EngineExecutionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hakudu.Bootstrapper.Engine
+{
+    public class EngineExecutionException : BootstrapperException
+    {
+        public EngineExecutionException()
+        {
+        }
+
+        public EngineExecutionException(string message) : base(message)
+        {
+        }
+
+        public EngineExecutionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/Hakudu.Bootstrapper/Engine/EngineHost.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Hakudu.Bootstrapper.LocalStorage;
+
+namespace Hakudu.Bootstrapper.Engine
+{
+    public class EngineHost
+    {
+        const string ENV_BOOTSTRAPPER_EXE = "HAKUDU_BOOTSTRAPPER_EXE";
+        const string ENV_BOOTSTRAPPER_VERSION = "HAKUDU_BOOTSTRAPPER_VERSION";
+
+        readonly BootstrapperContext _bootstrapper;
+
+        public EngineHost(BootstrapperContext bootstrapper)
+        {
+            if (bootstrapper == null)
+                throw new ArgumentNullException(nameof(bootstrapper));
+
+            _bootstrapper = bootstrapper;
+        }
+
+        public async Task<int> StartAsync(EngineInfo engine)
+        {
+            if (engine == null)
+                throw new ArgumentNullException(nameof(engine));
+
+            var startInfo = new ProcessStartInfo(engine.ExePath)
+            {
+                WorkingDirectory = engine.InstallDir.FullName,
+                UseShellExecute = false
+            };
+
+            SetEnvironmentVariables(startInfo.EnvironmentVariables);
+
+            return await StartProcessAsync(startInfo);
+        }
+
+        void SetEnvironmentVariables(StringDictionary env)
+        {
+            env[ENV_BOOTSTRAPPER_EXE] = _bootstrapper.ExePath;
+            env[ENV_BOOTSTRAPPER_VERSION] = _bootstrapper.Version.ToString();
+        }
+
+        static Task<int> StartProcessAsync(ProcessStartInfo startInfo)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            var process = new Process
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+
+            process.Exited += (sender, e) =>
+            {
+                tcs.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            try
+            {
+                if (!process.Start())
+                {
+                    var errorMessage = $"Failed to start the Hakudu engine process ({startInfo.FileName}).";
+                    tcs.SetException(new EngineExecutionException(errorMessage));
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = $"Failed to start the Hakudu engine process ({startInfo.FileName}). {ex.Message}";
+                tcs.SetException(new EngineExecutionException(errorMessage));
+            }
+
+            return tcs.Task;
+        }
+    }
+}
